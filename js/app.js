0ef3555939d2d303849cd5e5def6343df5b02c06
John <ema8720@gmail.com>
2019-01-03 22:04:36 -0800

    if else statement

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 // immediate invoked function expression
 (function() {
+    // setting up logic for every time we will be submitting the form.
+document.getElementById('message-form').addEventListener('submit', function(e) {
+    // prevent default action
+    e.preventDefault();
+    // getting a value
+const message = document.getElementById('message');
+const value = message.value;
 
+if (value === '') {
+    console.log('empty value');
+    
+}
+
+
+
+    // debugging and testing
+    console.log(value);
+    // take the message.value to the dom on h4 element
+document.querySelector('.message-content').textContent = value;
+message.value = '';
+
+})
 })();
